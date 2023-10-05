af50edaf35b58d1240dab9d29dcfa326a1b0ce2a
Rahmad Noor Ikhsan <rajaikhsan2021@gmail.com>
2023-10-05 15:05:00 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/waterseven/macro/lestari/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.waterseven.macro.lestari
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.waterseven.macro.lestari", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/waterseven/macro/lestari/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waterseven.macro.lestari
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
