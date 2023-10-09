5768099a04c52c53ecc5f133b64988ba31c07c1e
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2023-10-09 11:57:31 +0700

    Added Bottom Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/waterseven/macro/lestari/MainActivity.kt
+++ app/src/main/java/com/waterseven/macro/lestari/MainActivity.kt
@@ -2,10 +2,23 @@ package com.waterseven.macro.lestari
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.waterseven.macro.lestari.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnClickListener { item ->
+            when(item.id) {
+                R.id.homeFragment -> {}
+                R.id.watchFragment -> {}
+                R.id.communityFragment -> {}
+                R.id.profileFragment -> {}
+            }
+        }
     }
 }
