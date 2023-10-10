02a9fc14da17b5de5ac357b16678d68b49f3d459
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2023-10-10 15:08:09 +0700

    Clean Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
 buildscript {
-    repositories {
-        google()
-    }
     dependencies {
         val navVersion = "2.5.2"
         //noinspection GradleDependency
@@ -12,6 +9,4 @@ buildscript {
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("androidx.navigation.safeargs") version "2.5.2" apply false
-
 }
