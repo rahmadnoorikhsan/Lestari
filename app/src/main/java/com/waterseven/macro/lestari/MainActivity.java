3903b529dd15d87474376b4af632f60dc61df195
Aufal Muaffaq <muhammadaufalm234@gmail.com>
2023-10-11 21:37:47 +0700

    perbaikan lagi

---
+++ app/src/main/java/com/waterseven/macro/lestari/MainActivity.java
@@ -0,0 +1,6 @@
+package com.waterseven.macro.lestari;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/com/waterseven/macro/lestari/MainActivity.kt
+++ app/src/main/java/com/waterseven/macro/lestari/MainActivity.kt
@@ -1,11 +1,33 @@
-package com.waterseven.macro.lestari
+package com.example.task_androfund_aufalmuaffaq_mobileb_morning
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.waterseven.macro.lestari.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnImplicit: Button = findViewById(R.id.btn_Implicit)
+        btnImplicit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id. btn_Implicit -> {
+                val message = "Terima Kasih"
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                startActivity(intent)
+            }
+        }
     }
 }
