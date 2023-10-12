cf6fc6691a112884280f489ec5d9fcd8174193e6
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2023-10-12 11:48:10 +0700

    Repair Feature Community

--- app/src/main/java/com/waterseven/macro/lestari/data/culture/CulturesData.kt
+++ app/src/main/java/com/waterseven/macro/lestari/data/culture/CulturesData.kt
@@ -24,10 +24,20 @@ object CulturesData {
         ),
         Culture(
             id = 3,
-            title = "Bola Soba",
+            title = "Rumah Bola Soba",
             place = "Sulawesi Selatan",
             description = "Bola Soba adalah rumah adat yang berasal dari Sulawesi Selatan. Rumah ini merupakan rumah bangsawan dari suku Bugis. Tidak hanya itu, rumah Bola Soba juga merupakan rumah dari raja Bugis. Kata bola soba dalam Bahasa Indonesia berarti persahabatan jadi Rumah Bola Soba berarti rumah persahabatan. Bahan dasar dari rumah ini adalah kayu. Rumah ini mempunyai model seperti rumah panggung dengan beberapa tiang yang menyangga badan rumah. Pada tahun 1920, Rumah Bola Soba menjadi tempat penginapan bagi tamu-tamu yang berasal dari luar negeri seperti Belanda. Rumah Bola Soba memiliki panjang sekitar 40 meter. Teras rumah sepanjang 6 Meter, rumah induk sepanjang 21 Meter, dan bagian belakang rumah sepanjang 8 Meter. Bagian belakang rumah umumnya digunakan sebagai dapur. Saat ini, bagian dalam Rumah Bola Soba berisi barang-barang bersejarah berupa meriam dan berbagai peralatan perang. Saat ini, rumah ini digunakan seperti museum. Rumah Bola Soba juga sering digunakan untuk pertemuan-pertemuan adat.",
             imageCover = "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1616224329/ig31e1duulz0ph8anwkl.jpg",
+            imageAsset = listOf(),
+            video = ""
+        ),
+        Culture(
+            id = 4,
+            title = "Rumah Bubungan Lima",
+            place = "Bengkulu",
+            description = "Rumah Bubungan Lima adalah rumah adat dari provinsi Bengkulu. Rumah ini memiliki model seperti rumah panggung yang ditopang oleh beberapa tiang penopang. Rumah ini bukanlah rumah tinggal seperti pada umumnya. Biasanya rumah ini digunakan untuk acara adat masyarakat Bengkulu.\n" +
+                    "Rumah ini terbagi atas tiga bagian yaitu rumah bagian atas, rumah bagian tengah, dan rumah bagian bawah.[1] Rumah Bubungan Lima memiliki materi dasar yaitu kayu.[1] Kayu yang dipilih pun bukan kayu sembarangan melainkan kayu yang kuat dan tahan lama. Kayu yang biasanya digunakan untuk membangun Rumah Bubungan Lima adalah Kayu Medang Kemuning. Rumah Bubungan Lima dibangun tinggi agar menghindari pemilik rumah beserta keluarga dari serangan binatang liar dan juga dari bencana alam seperti banjir. Karena tinggi Rumah Bubungan Lima ini, maka orang-orang yang hendak masuk ke dalam rumah pun harus menggunakan tangga. Tangga yang digunakan untuk masuk ke dalam rumah umumnya mempunyai jumlah anak tangga yang ganjil sesuai dengan kepercayaan masyarakat Bengkulu. Rumah Bubungan Lima ini merupakan salah satu Budaya Indonesia yang menjadi objek wisata. Pada zaman dahulu rumah bubungan lima menjadi tempat tinggal para raja ataupun kaum bangsawan.",
+            imageCover = "https://img.iproperty.com.my/angel-legacy/1110x624-crop/static/2021/08/Mengenal-Rumah-Adat-Bengkulu-Bubungan-Lima-dan-Filosofinya.png",
             imageAsset = listOf(),
             video = ""
         )
