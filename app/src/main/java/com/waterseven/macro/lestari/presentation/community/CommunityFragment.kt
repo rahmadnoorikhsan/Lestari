8f6441a2f5d1265464c00d3e03391dc3eb22f8fd
Rifqieeee <nugraharifqi0@gmail.com>
2023-10-18 22:41:08 +0700

    Tambah Feature Profile

--- app/src/main/java/com/waterseven/macro/lestari/presentation/community/CommunityFragment.kt
+++ app/src/main/java/com/waterseven/macro/lestari/presentation/community/CommunityFragment.kt
@@ -1,15 +1,14 @@
 package com.waterseven.macro.lestari.presentation.community
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.waterseven.macro.lestari.R
 import com.waterseven.macro.lestari.databinding.FragmentCommunityBinding
-import androidx.core.content.ContextCompat
 
 class CommunityFragment : Fragment() {
 
